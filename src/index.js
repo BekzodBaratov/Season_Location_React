c0a94fc3757dbb72bd060798458053f439c6feac
bekzod <bekzodbaratov@gmail.com>
2022-05-04 13:56:31 +0500

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,38 +1,75 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import DisplaySeason from "./DisplaySeason";
+const Redux = require("redux");
 
-// Class componenta
-class App extends React.Component {
-  state = {
-    lat: null,
-    errorMessage: "",
+const addMusic = (name, time) => {
+  return {
+    type: "ADD",
+    payload: {
+      name,
+      time,
+    },
   };
-
-  componentDidMount() {
-    navigator.geolocation.getCurrentPosition(
-      (position) => this.setState({ lat: position.coords.latitude }),
-      (err) => this.setState({ errorMessage: err.message })
+};
+const removeMusic = (name, info) => {
+  return {
+    type: "DEL",
+    payload: {
+      name,
+      info,
+    },
+  };
+};
+const findMusic = (name, info) => {
+  return {
+    type: "FIND",
+    payload: {
+      name,
+      info,
+    },
+  };
+};
+/////////////////////////////////////////
+const addMusicReducer = (history = [], form) => {
+  if (form.type === "ADD") {
+    return (history = [...history, form]);
+  }
+  return history;
+};
+const findMusicReducer = (history = [], form) => {
+  if (form.type === "FIND") {
+    const musicName = form.payload.name;
+    const obj = form.payload.info.addM.find(
+      (val) => val.payload.name === musicName
     );
+    return obj.payload.time;
   }
-  componentDidUpdate() {
-    this.render();
-  }
-
-  getLoaction() {
-    if (this.state.latitude && !this.state.errorMessage) {
-      return <div>Latitude: {this.state.latitude}</div>;
-    } else if (this.state.errorMessage) {
-      return <div>Error: {this.state.errorMessage}</div>;
-    } else {
-      return <div>loading....</div>;
-    }
-  }
-  render() {
-    return (
-      <DisplaySeason lat={this.state.lat} error={this.state.errorMessage} />
+  return 1;
+};
+const delMusicReducer = (history = [], form) => {
+  if (form.type === "DEL") {
+    const musicName = form.payload.name;
+    const arr = form.payload.info.addM.filter(
+      (val) => val.payload.name !== musicName
     );
+    return arr;
   }
-}
+  return 1;
+};
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+///////////////////////////////////////////////////
+
+const { createStore, combineReducers } = Redux;
+const departaments = combineReducers({
+  addM: addMusicReducer,
+  findM: findMusicReducer,
+  delM: delMusicReducer,
+});
+
+const store = createStore(departaments);
+store.dispatch(addMusic("Uzbekistan", "3:44"));
+store.dispatch(addMusic("Uzbekistan1", "3:22"));
+store.dispatch(addMusic("Uzbekistan2", "3:11"));
+store.dispatch(findMusic("Uzbekistan2", store.getState()));
+store.dispatch(removeMusic("Uzbekistan1", store.getState()));
+store.dispatch(removeMusic("Uzbekistan", store.getState()));
+
+console.log(store.getState());
